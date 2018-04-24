461ee2c7cff9da6d7851bfb2ae442d2dd7235b1a
Keiji <keiji.ue@gmail.com>
2018-04-24 22:28:43 +0900

    add scratch_base

---
+++ scratch_base/create.sh
@@ -0,0 +1,15 @@
+# copy sh
+cp -pf /bin/sh .
+cp -pf /lib/x86_64-linux-gnu/libc.so.6 .
+cp -pf /lib64/ld-linux-x86-64.so.2 .
+
+# copy ls
+cp -pf /bin/ls .
+cp -pf /lib/x86_64-linux-gnu/libselinux.so.1 .
+cp -pf /lib/x86_64-linux-gnu/libpcre.so.3 .
+cp -pf /lib/x86_64-linux-gnu/libdl.so.2 .
+cp -pf /lib64/ld-linux-x86-64.so.2 .
+cp -pf /lib/x86_64-linux-gnu/libpthread.so.0 .
+sudo docker build -t scratch_base .
+sudo docker run -itd --rm --name scratch_base scratch_base sh
+sudo docker exec -it scratch_base sh
