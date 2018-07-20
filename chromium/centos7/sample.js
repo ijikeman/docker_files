b8e33ac613ee6be41d898fe93a37ac515cd6f8c4
keiji.uehata <keiji.uehta@rakus.co.jp>
2018-07-20 11:36:46 +0900

    fixed sample.js

--- chromium/centos7/sample.js
+++ chromium/centos7/sample.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async() => {
 
 //const browser = await puppeteer.launch();
-const browser = await puppeteer.launch({args: ['--no-sandbox']});
+const browser = await puppeteer.launch({args: ['--no-sandbox'],  executablePath: '/nvm/node_modules/puppeteer/.local-chromium/linux-564778/chrome-linux/chrome'});
 const page = await browser.newPage();
 await page.goto('https://example.com');
 await page.screenshot({path: 'example.png'});
